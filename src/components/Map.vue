4b0ba845122e7a19b0010a24ba094c533542ed77
arnau <arnau.badenas.7e6@itb.cat>
2023-05-02 12:04:07 +0200

    map component

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="height:600px; width:800px">
+        <!-- TODO salir que esté por defecto en bcn con el zoom adecuado -->
+        <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+            <l-tile-layer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    layer-type="base"
+                    name="OpenStreetMap"
+            ></l-tile-layer>
+        </l-map>
+    </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+    name: 'map-component',
+    components: {
+        LMap,
+        LTileLayer,
+    },
+    data() {
+        return {
+            zoom: 12,
+        };
+    },
+};
+</script>
+
+<style></style>
