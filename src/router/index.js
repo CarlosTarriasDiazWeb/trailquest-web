02e2909343c5b938dd9a1e3ff1444ab936913b81
carlos.tarrias.7e6 <carlos.tarrias.7e6@itb.cat>
2023-05-02 08:20:21 +0200

    Creada estrucutura inicial del proyecto con vue-router

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// router.beforeEach(async (to) => {
+//   if (
+//     // make sure the user is authenticated
+//     document.cookie === "" &&
+//     // ❗️ Avoid an infinite redirect
+//     to.name === "users"
+//   ) {
+//     // redirect the user to the login page
+//     return { name: "login" };
+//   }
+//   if (document.cookie !== "" && to.name === "login") {
+//     return { name: "users" };
+//   }
+// });
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+   <h1>Home</h1>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomeView',
+    props: {
+     
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+
+  </style>
+  
